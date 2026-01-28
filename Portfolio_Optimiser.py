a7aef6be9834490c1492f85f68a589f0a0302cfa
Pradeep Selvaraj <pradeep.selvaraj.23@gmail.com>
2026-01-28 19:02:04 +0000

    The following updates have been made in the commit:
    - Moved configurable params on top.
    - Combined utility functions in 1 section.
    - Dataframe concatenation inside nested loops - Bad idea (O(n2)). Replaced with python dicts and daraframe built at end.
    - Fixed skiprows hardcoding in csv reading and implemented dynamic logic.
    - Variable names updated for ease of understanding.
    - Total 3 csvs are dumped - Holdings, Movement, Final report.

--- Portfolio_Optimiser.py
+++ Portfolio_Optimiser.py
@@ -1,24 +1,48 @@
 import os
-import pandas as pd
 import re
-from rapidfuzz import process, fuzz
 from collections import defaultdict
 
-Excluded_Stocks = ['SK Hynix Inc',
-                   'Amazon.com Inc',
-                   'Alibaba Group Holding Limited',
-                   'Contemporary Amperex Technology Co Limited',
-                   'NIKE Inc',
-                   'NVIDIA CORP',
-                   'Sony group',
-                   'Sony Financial Holdings INC NPV',
-                   'Innoventive Industries Limited**',
-                   'Epam Systems Inc',
-                   'Cognizant Technology Solutions Corporation',
-                   'Disa India Ltd.',
-                   '0.00%  FCDW -  BETA NAPTHOL LT',
-                   'EQ - ELBEE SERVICES LTD.(ELBEE EXPR',
-                   'EQ - GPI TEXTILES LTD.']
+import pandas as pd
+
+# ---------------------------
+# Configuration
+# ---------------------------
+EXCLUDED_STOCKS = [
+    'SK Hynix Inc', 'Amazon.com Inc', 'Alibaba Group Holding Limited',
+    'Contemporary Amperex Technology Co Limited', 'NIKE Inc', 'NVIDIA CORP',
+    'Sony group', 'Sony Financial Holdings INC NPV',
+    'Innoventive Industries Limited**', 'Epam Systems Inc',
+    'Cognizant Technology Solutions Corporation', 'Disa India Ltd.',
+    '0.00%  FCDW -  BETA NAPTHOL LT', 'EQ - ELBEE SERVICES LTD.(ELBEE EXPR',
+    'EQ - GPI TEXTILES LTD.'
+]
+
+MAX_PREFIX_LENGTH = 20
+TICKER_CSV_PATH = "nse_ticker.csv"
+
+# ---------------------------
+# Utility Functions
+# ---------------------------
+
+def clean_stock_name(stock_name):
+    """Cleans and standardizes the stock name."""
+    if isinstance(stock_name, str):
+        stock_name = re.sub(r"^EQ(?=\s|-)", "", stock_name, flags=re.IGNORECASE)
+        stock_name = re.sub(r"^EQ\s*\(RTS-PP\)", "", stock_name, flags=re.IGNORECASE)
+        stock_name = re.sub(r"&", "and", stock_name, flags=re.IGNORECASE)
+        stock_name = re.sub(r"[^A-Za-z0-9]", "", stock_name)
+    return stock_name
+
+def safe_float(value):
+    """Safely converts value to float."""
+    try:
+        return float(value)
+    except ValueError:
+        return None
+    
+# ---------------------------
+# CSV Processing
+# ---------------------------
 
 def process_csv_files(main_csv_path):
     # Read the main CSV file
@@ -26,20 +50,11 @@ def process_csv_files(main_csv_path):
     
     # Folder paths
     folder_path = os.path.dirname(main_csv_path)
-    # small_folder_path = os.path.join(folder_path, "Small")
     
     # Initialize output DataFrames
-    output_df = pd.DataFrame(columns=['Stocks'])
-    output_df_change = pd.DataFrame(columns=['Stocks'])
-
-    def clean_stock_name(stock_name):
-        """Cleans and standardizes the stock name."""
-        if isinstance(stock_name, str):
-            stock_name = re.sub(r"^EQ(?=\s|-)", "", stock_name, flags=re.IGNORECASE)
-            stock_name = re.sub(r"^EQ\s*\(RTS-PP\)", "", stock_name, flags=re.IGNORECASE)
-            stock_name = re.sub(r"&", "and", stock_name, flags=re.IGNORECASE)
-            stock_name = re.sub(r"[^A-Za-z0-9]", "", stock_name)
-        return stock_name
+    Holdings = {}
+    Movement = {}
+    Report = {}
 
     ticker_df = pd.read_csv("nse_ticker.csv")
     ticker_df["CompanyName_clean"] = (
@@ -62,11 +77,8 @@ def process_csv_files(main_csv_path):
 
     prefix_index = defaultdict(list)
 
-    # Build index for prefixes up to reasonable length (say 10)
-    MAX_PREFIX = 100
-
     for name in company_names:
-        for i in range(2, min(len(name), MAX_PREFIX) + 1):
+        for i in range(2, min(len(name), MAX_PREFIX_LENGTH) + 1):
             prefix_index[name[:i]].append(name)
 
     def prefix_match_ticker(stock_name):
@@ -78,7 +90,7 @@ def process_csv_files(main_csv_path):
         if len(stock) < 2:
             return None
 
-        for i in range(2, min(len(stock), MAX_PREFIX) + 1):
+        for i in range(2, min(len(stock), MAX_PREFIX_LENGTH) + 1):
             prefix = stock[:i]
             matches = prefix_index.get(prefix)
 
@@ -90,60 +102,79 @@ def process_csv_files(main_csv_path):
 
         return None
 
-    def safe_float(value):
-        """Safely converts value to float."""
-        try:
-            return float(value)
-        except ValueError:
-            return None
-
     # Process each fund
     for fund_name in main_df['Funds']:
         file_path = os.path.join(folder_path, f"{fund_name.replace(' ', '_').replace('/', '_')}.csv")
 
         if not os.path.exists(file_path):
             continue
+        
+        # Read raw CSV with no header
+        header_row = 2
+        preview = pd.read_csv(file_path, header=header_row)
+        if preview.iloc[1].astype(str).str.contains("% of AUM", case=False, na=False).any():
+            header_row = header_row + 1
+        fund_df = pd.read_csv(file_path, header=header_row)
 
-        fund_df = pd.read_csv(file_path, header=None, skiprows=4)
-        if fund_name not in output_df.columns:
-            output_df[fund_name] = None
-            output_df_change[fund_name] = None
-
-        processed_stocks = set()
-        processed_stocks_change = set()
-
-        for _, row in fund_df.iterrows():
-            if row.isnull().any():
+        for _, col in fund_df.iterrows():
+            if col.isnull().any():
                 print(f"Empty row found in {fund_name}, skipping the rest of the file.")
                 break
 
-            stock_name, stock_value, col3, col5 = row[0], row[1], safe_float(row[2]), safe_float(row[4])
-            if stock_name not in Excluded_Stocks:    
+            stock_name, stock_holding, col3, col5 = col.iloc[0], col.iloc[1], safe_float(col.iloc[2]), safe_float(col.iloc[4])
+            if stock_name not in EXCLUDED_STOCKS:    
                 if col3 is None:
                     continue
                 change = col3 if col5 is None else col3 - col5
 
-                stock_name = prefix_match_ticker(stock_name)
+                Ticker = prefix_match_ticker(stock_name)
+                company_name = ticker_df.loc[ticker_df['Symbol'] == Ticker, 'CompanyName'].values[0]
 
-                if stock_name not in processed_stocks:
-                    if stock_name.lower() not in output_df['Stocks'].str.lower().tolist():
-                        output_df = pd.concat([output_df, pd.DataFrame({'Stocks': [stock_name], fund_name: [stock_value]})], ignore_index=True)
-                    else:
-                        output_df.loc[output_df['Stocks'].str.lower() == stock_name.lower(), fund_name] = stock_value
-                    processed_stocks.add(stock_name)
+                # -------- Holdings / Changes --------
+                if Ticker not in Holdings:
+                    Holdings[Ticker] = {
+                        'Ticker': Ticker,
+                        'Stock': company_name
+                    }
+                    Movement[Ticker] = {
+                        'Ticker': Ticker,
+                        'Stock': company_name
+                    }
+                    Report[Ticker] = {
+                        'Ticker': Ticker,
+                        'Stock': company_name
+                    }
+                
+                Holdings[Ticker][fund_name] = stock_holding
+                Movement[Ticker][fund_name] = change
 
-                if stock_name not in processed_stocks_change:
-                    if stock_name.lower() not in output_df_change['Stocks'].str.lower().tolist():
-                        output_df_change = pd.concat([output_df_change, pd.DataFrame({'Stocks': [stock_name], fund_name: [change]})], ignore_index=True)
-                    else:
-                        output_df_change.loc[output_df_change['Stocks'].str.lower() == stock_name.lower(), fund_name] = change
-                    processed_stocks_change.add(stock_name)
+    for ticker, funds in Holdings.items():
+        total_holdings = sum(float(value) for key, value in funds.items()
+                             if key not in ['Ticker', 'Stock', 'Total Holdings'])
+        Report[ticker]["Total holdings"] = total_holdings
+    
+    for ticker, funds in Movement.items():
+        total_movement = sum(float(value) for key, value in funds.items()
+                            if key not in ['Ticker', 'Stock', 'Total Holdings'])
+        Report[ticker]["Total Movement"] = total_movement
+    
+    grand_total = sum(data["Total holdings"] for data in Report.values())
+    for ticker in Report:
+        Report[ticker]["Normalised holdings"] = (
+            Report[ticker]["Total holdings"] / grand_total) * 100
 
     # Save output files
-    output_df.to_csv(os.path.join(folder_path, '12-25.csv'), index=False)
+    output_df_holdings = pd.DataFrame.from_dict(Holdings, orient='index')
+    output_df_movement = pd.DataFrame.from_dict(Movement, orient='index')
+    output_df_report = pd.DataFrame.from_dict(Report, orient='index')
+
+    output_df_holdings.to_csv(os.path.join(folder_path, '12_25_holdings.csv'), index=False)
     print(f"Output saved to 12-25.csv")
 
-    output_df_change.to_csv(os.path.join(folder_path, '12-25_change.csv'), index=False)
+    output_df_movement.to_csv(os.path.join(folder_path, '12_25_movement.csv'), index=False)
+    print(f"Output saved to 12-25_change.csv")
+
+    output_df_report.to_csv(os.path.join(folder_path, '12_25_report.csv'), index=False)
     print(f"Output saved to 12-25_change.csv")
 
 # Example usage
